f6b7664dde2486129b29b20b9f9a01a60aff43b9
dawnvt <64136899+dawnvt@users.noreply.github.com>
2021-08-06 10:24:37 +0200

    Forgot some project files. Damn.

---
+++ TimeDeviance/Configuration/PluginConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+using IPA.Config.Stores;
+
+[assembly: InternalsVisibleTo(GeneratedStore.AssemblyVisibilityTarget)]
+namespace TimeDeviance.Configuration
+{
+    public class PluginConfig
+    {
+        public virtual bool Enabled { get; set; }
+    }
+}
---
+++ TimeDeviance/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using IPA;
+using SiraUtil.Zenject;
+using IPALogger = IPA.Logging.Logger;
+using IPA.Config.Stores;
+using TimeDeviance.Installers;
+using Config = IPA.Config.Config;
+
+namespace TimeDeviance
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        [Init]
+        public void Init(IPALogger logger, Zenjector zenjector, Config config)
+        {
+            zenjector.OnGame<GameInstaller>().ShortCircuitForMultiplayer();
+        }
+
+        [OnEnable, OnDisable]
+        public void OnChange()
+        {
+        }
+    }
+}
